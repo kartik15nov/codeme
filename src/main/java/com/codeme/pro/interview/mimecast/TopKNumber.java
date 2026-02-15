f2654b4668bc1982b59f8341c3ed2165aab1a534
Kartikeswar Mallik <31155935+kartik15nov@users.noreply.github.com>
2026-02-15 09:31:32 +0530

    changed return type

--- src/main/java/com/codeme/pro/interview/mimecast/TopKNumber.java
+++ src/main/java/com/codeme/pro/interview/mimecast/TopKNumber.java
@@ -1,8 +1,8 @@
 package com.codeme.pro.interview.mimecast;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -12,10 +12,10 @@ public class TopKNumber {
     System.out.println("Hello");
 
     int[] arr = new int[]{-1, 0, 2, 2, 2, 2, 3, 3, 3, 1, 1, 2};
-    System.out.println(getTopK(arr, 2));
+    System.out.println(Arrays.toString(getTopK(arr, 2)));
   }
 
-  public static List<Integer> getTopK(int[] arr, int k) {
+  public static int[] getTopK(int[] arr, int k) {
     Map<Integer, Integer> map = new HashMap<>();
 
     for (int j : arr) {
@@ -26,7 +26,8 @@ public class TopKNumber {
         .sorted(Entry.comparingByValue(Comparator.reverseOrder()))
         .map(Entry::getKey)
         .limit(k)
-        .toList();
+        .mapToInt(Integer::intValue)
+        .toArray();
   }
 
   public static int[] getTopK1(int[] arr, int k) {
